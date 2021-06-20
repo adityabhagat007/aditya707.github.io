b79e47d9e8b294da7009579b154951e610465fa3
adityabhagat007 <rajarambhagat6582@gmail.com>
2021-06-20 20:13:04 +0530

    adding style

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,7 @@
 console.log("welcome to the console :)");
 ScrollReveal().reveal('.card1', { delay: 500 });
 ScrollReveal().reveal('.my_project_card ', { delay: 500 });
+ScrollReveal().reveal('.heading1', { delay: 500 });
+ScrollReveal().reveal('.About', { delay: 500 });
+ScrollReveal().reveal('.email', { delay: 500 });
+ScrollReveal().reveal('.emailid', { delay: 500 });
