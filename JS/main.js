ca62a6dea45e645944b8adc11f9aa0a62c09db31
adityabhagat007 <rajarambhagat6582@gmail.com>
2021-06-30 02:27:37 +0530

    Typed effect

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,13 @@ ScrollReveal().reveal('.heading1', { delay: 500 });
 ScrollReveal().reveal('.About', { delay: 500 });
 ScrollReveal().reveal('.email', { delay: 500 });
 ScrollReveal().reveal('.emailid', { delay: 500 });
+
+const typed = new Typed('.main_header2',{
+    stringsElement:'#typed-string',
+    typeSpeed:100,
+    loop:true,
+    startDelay:700,
+    backDelay:1000,
+    showCursor:false,
+    backSpeed:20,
+}) 
